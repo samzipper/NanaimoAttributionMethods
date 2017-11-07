881616957f6dad4e2eaa2a7f49abaeb5dd2dbefa
Sam Zipper <samuelczipper@gmail.com>
2017-11-06 16:00:47 -0800

    Add aggregated raw streamflow depletion results

---
+++ ProcessingScripts/DepletionByWell_01_AggregateAllResults.R
@@ -0,0 +1,56 @@
+## DepletionByWell_01_AggregateAllResults.R
+#' This script is intended to aggregate streamflow depletion estimates for
+#' all drainage densities, topographies, recharge values, and models.
+#' 
+#' This script should only have to be run once, unless it is necessary
+#' to update Tom D's raw model results on the GSAS directory for some reason.
+
+rm(list=ls())
+
+# directory to repository on local computer
+dir.git <- "C:/Users/Sam/WorkGits/NanaimoAttributionMethods/"
+
+# load paths + packages
+source(paste0(dir.git, "ProcessingScripts/paths+packages.R"))
+
+# list of all CSV files
+files.all <- list.files(paste0(dir.GSAS, "DepletionByWell/"))
+
+# load files
+start.flag <- T
+for (file in files.all){
+  # parse name
+  traits <- str_split(tools::file_path_sans_ext(file), pattern="_", n=4, simplify=T)
+  
+  # load file
+  df.in <- read.csv(paste0(dir.GSAS, "DepletionByWell/", file))
+  
+  # replace well string with numeric
+  df.in$Well <- as.numeric(substring(df.in$Well, first=5))
+  
+  # melt to longform
+  df.in.melt <- melt(df.in, id=c("Well"), value.name="depletion.prc", variable.name="Zone")
+  
+  # replace zone string with numeric
+  df.in.melt$Zone <- as.numeric(substring(df.in.melt$Zone, first=5))
+  
+  # add relevant output data
+  df.in.melt$drainage.density <- traits[1]
+  df.in.melt$topography <- traits[2]
+  df.in.melt$recharge <- traits[3]
+  df.in.melt$method <- traits[4]
+  
+  # output data frame
+  if (start.flag){
+    df.all <- df.in.melt
+    start.flag <- F
+  } else {
+    df.all <- rbind(df.all, df.in.melt)
+  }
+  
+  # status update
+  print(paste0(file, " complete"))
+}
+
+# save output
+write.csv(df.all, paste0(dir.git, "data/DepletionByWell_01_AggregateAllResults.csv"), row.names=F)
---
+++ ProcessingScripts/paths+packages.R
@@ -0,0 +1,13 @@
+## paths+packages.R
+#' This script contains relevant paths and packages common to multiple processing scripts.
+
+## packages
+require(dplyr)
+require(ggplot2)
+require(hydroGOF)
+require(stringr)
+require(reshape2)
+
+## paths
+# path to directory on GSAS with data
+dir.GSAS <- "Z:/2.active_projects/Zipper/2.Model_data/NanaimoAttributionMethods/"
